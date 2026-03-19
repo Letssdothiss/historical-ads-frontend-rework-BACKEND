22d08976cb63dcad19db44ae5070e8d054ece795
Emil Meri <em223ve@student.lnu.se>
2026-03-19 10:38:33 +0100

    Add config to runner

--- run.py
+++ run.py
@@ -1,4 +1,9 @@
 import uvicorn
 
+# Config
+APP_HOST="0.0.0.0"
+APP_PORT=8083
+APP_MODULE="app.main:app"
+
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8083, reload=True)
+    uvicorn.run(APP_MODULE, host=APP_HOST, port=APP_PORT, reload=True)
