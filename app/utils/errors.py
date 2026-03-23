595d0cebe751a044900e2a41a61e7cc8334a313f
Rouni <ra223aa@student.lnu.se>
2026-03-23 17:52:45 +0100

    Add NotFoundError class for improved error handling

--- app/utils/errors.py
+++ app/utils/errors.py
@@ -24,4 +24,12 @@ class BadRequestError(APIError):
       detail=detail,
       error_code="BadRequestError"
     )
+# 404 Not Found
+class NotFoundError(APIError):
+  def __init__(self, detail: str = "Resource not found."): 
+    super().__init__(
+      status_code=status.HTTP_404_NOT_FOUND,
+      detail=detail,
+      error_code="NotFoundError"
+    )
     
