1302d2158d1bae2f1f0173cf1ec799f7815dc3dd
Rouni <ra223aa@student.lnu.se>
2026-03-23 17:51:33 +0100

    Add BadRequestError class for improved error handling

--- app/utils/errors.py
+++ app/utils/errors.py
@@ -4,6 +4,7 @@ Custom error classes for the application.
 from typing import Optional, Dict, Any
 from fastapi import HTTPException, status
 
+# 500 Internal Server Error
 class APIError(HTTPException):
     """Base application error"""
     def __init__(
@@ -15,4 +16,12 @@ class APIError(HTTPException):
     ):
         self.error_code = error_code or self.__class__.__name__
         super().__init__(status_code=status_code, detail=detail, headers=headers)
-        
+# 400 Bad Request
+class BadRequestError(APIError):
+  def __init__(self, detail: str = "Bad request."): 
+    super().__init__(
+      status_code=status.HTTP_400_BAD_REQUEST,
+      detail=detail,
+      error_code="BadRequestError"
+    )
+    
